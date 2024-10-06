f07ee9835640fd901b6f935bb9c871388b4116b2
Aarush Pothula <aarush.pothula@gmail.com>
2024-10-06 13:58:34 +0200

    formatting

--- Lessen/opdracht 4/4e.php
+++ Lessen/opdracht 4/4e.php
@@ -10,55 +10,53 @@ TIP
 Schrijf eerst eens op papier, met drie getallen a, b en c hoe je dat gaat aan pakken.
 -->
 <html>
+
 <head>
-<title>Untitled</title>
+	<title>Untitled</title>
 </head>
+
 <body>
-  <font face="verdana" size="+2" color="indigo">
-    	<h2 class="kop"><i>Uw invoer vergeleken:</i></h3>
-    				<?php
-                  $getal1=$_POST["getal1Veld"];
-									$getal2=$_POST["getal2Veld"];
-									$getal3=$_POST["getal3Veld"]; 
-									$ongeldiginvoer=false;
-									//validating input
-										if(!is_numeric($getal1)){
-											  print("<p>Getal a is ongeldig invoer, voer de volgende keer valide getallen in</p>");
-												$ongeldiginvoer=true;
-										}
-										if(!is_numeric($getal2)){
-											  print("<p>Getal b is ongeldig invoer, voer de volgende keer valide getallen in</p>");
-												$ongeldiginvoer=true;
-										}
-										if(!is_numeric($getal3)){
-											  print("<p>Getal c is ongeldig invoer, voer de volgende keer valide getallen in</p>");
-												$ongeldiginvoer=true;
-										}
-										if($getal1 == $getal2 OR $getal1 == $getal3 OR $getal2 == $getal3){
-    										print("<p>Ongeldig invoer, voer de volgende keer 3 verschillende getallen in</p>");
-												$ongeldiginvoer=true;
-  									}
-									//comparing numbers
-										if(!$ongeldiginvoer){
-    									if($getal1 > $getal2 && $getal2 > $getal3){
-    									    print("<p>Getal a: <b>$getal1</b> is het grootst en getal c: <b>$getal3</b> het kleinst</p>");
-  										}						
-  										elseif($getal1 > $getal3 && $getal3 > $getal2){
-    											print("<p>Getal a: <b>$getal1</b> is het grootst en getal b: <b>$getal2</b> het kleinst</p>");
-  										}
-  										elseif($getal2 > $getal1 && $getal1 > $getal3){
-    											print("<p>Getal b: <b>$getal2</b> is het grootst en getal c: <b>$getal3</b> het kleinst</p>");
-  										}
-  										elseif($getal2 > $getal3 && $getal3 > $getal1){
-    									    print("<p>Getal b: <b>$getal2</b> is het grootst en getal a: <b>$getal1</b> het kleinst</p>");
-  										}
-  										elseif($getal3 > $getal2 && $getal2 > $getal1){
-    									    print("<p>Getal c: <b>$getal3</b> is het grootst en getal a: <b>$getal1</b> het kleinst</p>");
-  										}
-  										else{
-    									    print("<p>Getal c: <b>$getal3</b> is het grootst en getal b: <b>$getal2</b> het kleinst</p>");
-  										}
-										}
-      				?>
+	<font face="verdana" size="+2" color="indigo">
+		<h2 class="kop"><i>Uw invoer vergeleken:</i></h3>
+			<?php
+			$getal1 = $_POST["getal1Veld"];
+			$getal2 = $_POST["getal2Veld"];
+			$getal3 = $_POST["getal3Veld"];
+			$ongeldiginvoer = false;
+			//validating input
+			if (!is_numeric($getal1)) {
+				print("<p>Getal a is ongeldig invoer, voer de volgende keer valide getallen in</p>");
+				$ongeldiginvoer = true;
+			}
+			if (!is_numeric($getal2)) {
+				print("<p>Getal b is ongeldig invoer, voer de volgende keer valide getallen in</p>");
+				$ongeldiginvoer = true;
+			}
+			if (!is_numeric($getal3)) {
+				print("<p>Getal c is ongeldig invoer, voer de volgende keer valide getallen in</p>");
+				$ongeldiginvoer = true;
+			}
+			if ($getal1 == $getal2 or $getal1 == $getal3 or $getal2 == $getal3) {
+				print("<p>Ongeldig invoer, voer de volgende keer 3 verschillende getallen in</p>");
+				$ongeldiginvoer = true;
+			}
+			//comparing numbers
+			if (!$ongeldiginvoer) {
+				if ($getal1 > $getal2 && $getal2 > $getal3) {
+					print("<p>Getal a: <b>$getal1</b> is het grootst en getal c: <b>$getal3</b> het kleinst</p>");
+				} elseif ($getal1 > $getal3 && $getal3 > $getal2) {
+					print("<p>Getal a: <b>$getal1</b> is het grootst en getal b: <b>$getal2</b> het kleinst</p>");
+				} elseif ($getal2 > $getal1 && $getal1 > $getal3) {
+					print("<p>Getal b: <b>$getal2</b> is het grootst en getal c: <b>$getal3</b> het kleinst</p>");
+				} elseif ($getal2 > $getal3 && $getal3 > $getal1) {
+					print("<p>Getal b: <b>$getal2</b> is het grootst en getal a: <b>$getal1</b> het kleinst</p>");
+				} elseif ($getal3 > $getal2 && $getal2 > $getal1) {
+					print("<p>Getal c: <b>$getal3</b> is het grootst en getal a: <b>$getal1</b> het kleinst</p>");
+				} else {
+					print("<p>Getal c: <b>$getal3</b> is het grootst en getal b: <b>$getal2</b> het kleinst</p>");
+				}
+			}
+			?>
 </body>
+
 </html>
